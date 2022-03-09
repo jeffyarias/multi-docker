54d7b2f77b5ede9a16c82edcc107987ca48baf8d
Jeffy Arias <jeff@Jeffys-MacBook-Pro.local>
2022-03-08 20:51:37 -0600

    complex deployment to aws

--- client/src/Fib.js
+++ client/src/Fib.js
@@ -28,7 +28,7 @@ class Fib extends Component {
   handleSubmit = async (event) => {
     event.preventDefault();
 
-    await axios.get('/api/values', {
+    await axios.post('/api/values', {
       index: this.state.index,
     });
     this.setState({ index: '' });
